6da2a2e34600ba69a14d78e2771a29bb2332f7ab
Kellie Mogg <kelliedmogg@gmail.com>
2021-02-16 22:39:12 +0000

    test files for console and file storage

---
+++ tests/test_models/test_engine/test_file_storage.py
@@ -0,0 +1,28 @@
+#!/usr/bin/python3
+""" module containts unittests for class BaseModel """
+
+
+import unittest
+import json
+from models.base_model import BaseModel
+from models.engine.file_storage import FileStorage
+
+class testFileStorage(unittest.TestCase):
+    """ unittests for File Storage """
+
+    def test_created_at_storage(self):
+        """ Datetime at creation of an object when stored """
+
+    def test_updated_at_storage(self):
+        """ Datetime at update of an object when stored """
+
+    def test_to_dict(self):
+        """ Test that keys/values are saving correctly """
+
+    def test_save(self):
+        """ Tests that instance attributes are saved and updated
+        correctly with the proper updated datetime """
+
+
+if __name__ == "__main__":
+    testFileStorage()
