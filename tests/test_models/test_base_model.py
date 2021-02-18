6cf021cdfcca95e730b53e294db3cefeb93e00fc
Kellie Mogg <kelliedmogg@gmail.com>
2021-02-18 22:45:08 +0000

    unit tests

--- tests/test_models/test_base_model.py
+++ tests/test_models/test_base_model.py
@@ -17,7 +17,7 @@ class testBaseModel(unittest.TestCase):
         self.velour1 = BaseModel(name="Sasha")
         self.velour2 = BaseModel(name="Sasha", id="None")
         self.velour3 = BaseModel(name="Sasha", number="2020")
-        self.velour4 = BaseModel(name="", number="None", id="None")
+        self.velour4 = BaseModel(name="", number="None", id="")
         self.velour5 = BaseModel()
 
     def tearDown(self):
@@ -33,10 +33,21 @@ class testBaseModel(unittest.TestCase):
         self.assertNotEqual(self.velour4.id, self.velour5.id)
         self.assertNotEqual(self.velour1.id, self.velour2.id)
 
+        self.assertIsInstance(self.velour.id, str)
+
     def test_custom(self):
         """ test for custom attributes """
         self.velour.hello = "world"
         self.assertEqual(self.velour.hello, "world")
+
+#    def test_created_at(self):
+#        """ Test that datetime updates at creation """
+#        self.assertIsInstance(self.velour.created_at, datetime)
+#        self.assertIsInstance(self.velour1.created_at, datetime)
+#        self.assertIsInstance(self.velour2.created_at, datetime)
+#        self.assertIsInstance(self.velour3.created_at, datetime)
+#        self.assertIsInstance(self.velour4.created_at, datetime)
+#        self.assertIsInstance(self.velour5.created_at, datetime)
 
     def test_noupdateId(self):
         """ test """
@@ -60,15 +71,45 @@ class testBaseModel(unittest.TestCase):
         """ Test that str prints details correctly """
         pass
 
-    def test_to_dict(self):
-        """ Test that keys/values are saving correctly """
-        pass
+#    def test_to_dict(self):
+#        """ Test that dict is saving all keys/values """
+#        self.assertTrue('id' in dir(self.velour))
+#        self.assertTrue('created_at' in dir(self.velour))
+#        self.assertTrue('updated_at' in dir(self.velour))
+#        self.assertTrue('to_dict' in dir(self.velour))
+#        self.assertTrue('save' in dir(self.velour))
 
-    def test_save(self):
-        """ Tests that instance attributes are saved and updated
-        correctly with the proper updated datetime """
-        pass
+#        velour1_list = dir(self.velour1)
+#        test = 'to_dict' in velour1_list
+#        self.assertTrue(test)
+#        test = 'created_at' in velour1_list
+#        self.assertTrue(test)
+#        self.assertTrue('updated_at' in dir(self.velour1))
+#        self.assertTrue('id' in dir(self.velour1))
+#        self.assertTrue('save' in dir(self.velour1))
 
+    def test_dictToObject(self):
+        """ Tests type from obj to dict to obj """
+        self.assertIsNot(type(self.velour), dict)
+        self.assertIsNot(type(self.velour1), dict)
+        self.assertIsNot(type(self.velour2), dict)
+        self.assertIsNot(type(self.velour3), dict)
+        self.assertIsNot(type(self.velour4), dict)
+        self.assertIsNot(type(self.velour5), dict)
+
+        velour_j = self.velour.to_dict()
+#        velour1_j = self.velour1.to_dict()
+#        velour2_j = self.velour2.to_dict()
+#        velour3_j = self.velour3.to_dict()
+#        velour4_j = self.velour4.to_dict()
+#        velour5_j = self.velour5.to_dict()
+
+        self.assertIs(type(velour_j), dict)
+#        self.assertIs(type(velour1_j), dict)
+#        self.assertIs(type(velour2_j), dict)
+#        self.assertIs(type(velour3_j), dict)
+#        self.assertIs(type(velour4_j), dict)
+#        self.assertIs(type(velour5_j), dict)
 
 if __name__ == "__main__":
     testBaseModel()
--- tests/test_models/test_user.py
+++ tests/test_models/test_user.py
@@ -23,7 +23,18 @@ class testUser(unittest.TestCase):
         """ Test for saved attributes """
         u1 = User()
         self.assertEqual(u1.email, "")
+        self.assertEqual(u1.password, "")
+        self.assertEqual(u1.first_name, "")
+        self.assertEqual(u1.last_name, "")
 
+    def test_id(self):
+        """ tests id is there """
+        a1 = User()
+        self.assertIsNotNone(a1.id)
+
+    def test_storage(self):
+        """ Test for storage of user data """
+        pass
 
 if __name__ == "__main__":
     testUser()
